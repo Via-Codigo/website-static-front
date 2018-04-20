bb5490f6e3668db62ed49c75c80e5c3502263184
ondoheer <ondoheer@gmail.com>
2018-04-20 12:41:30 -0500

    Avanzando componentes de títulos

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
 const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
@@ -36,6 +39,14 @@ module.exports = {
       template: "./src/index.html",
       filename: "./index.html"
     }),
+    new HtmlWebPackPlugin({
+      template: "./src/style-guide.html",
+      filename: "./style-guide.html"
+    }),
+    new CopyWebpackPlugin([
+      { from: "src/index.html", to: path.join(__dirname, "dist") },
+      { from: "src/style-guide.html", to: path.join(__dirname, "dist") }
+    ]),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
