bed1ccb0898aea28b8a98df5a734044e3b650219
ondoheer <ondoheer@gmail.com>
2018-04-21 00:04:06 -0500

    Se hace que c-lonely-link valga para etiquetas button

--- dist/style.f956e67b.js
+++ dist/style.f956e67b.js
@@ -144,7 +144,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":4}],55:[function(require,module,exports) {
+},{"_css_loader":4}],56:[function(require,module,exports) {
 
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -313,5 +313,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[55])
+},{}]},{},[56])
 //# sourceMappingURL=/style.f956e67b.map
